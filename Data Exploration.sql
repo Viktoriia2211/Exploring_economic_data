2ef8e3dbf53c86a09c48c46c92cb99085ea13ec9
Viktoriia_Popovych <125656597+Viktoriia2211@users.noreply.github.com>
2023-09-06 10:07:34 +0200

    Create Data Exploration.sql

---
+++ Data Exploration.sql
@@ -0,0 +1,6 @@
+# Here is an example data analyst project using the provided data sources with 10 sample SQL queries and visualization ideas:
+
+# Project Goal:
+  Analyze country data to identify relationships between tourism, standard of living, unemployment, corruption and GDP. Look for insights to guide business expansion decisions.
+
+Query 1: Join corruption and unemployment data to find average unemployment rate by corruption index
