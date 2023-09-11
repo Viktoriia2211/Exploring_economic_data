ca0c89e852483ce72a62f1856329b88ac2c70e74
Viktoriia_Popovych <125656597+Viktoriia2211@users.noreply.github.com>
2023-09-11 18:21:57 +0300

    Update Data Exploration.sql

--- Data Exploration.sql
+++ Data Exploration.sql
@@ -27,8 +27,66 @@ FROM unemployment u
 JOIN corruption c ON u.country = c.country
 
 -- Join corruption and unemployment data to find average unemployment rate by corruption index.
+
+
+    SELECT 
+  c.corruption_index, 
+  AVG(u.unemployment_rate) AS avg_unemployment_rate
+FROM corruption c
+INNER JOIN unemployment u 
+  ON c.country = u.country 
+GROUP BY c.corruption_index
+ORDER BY c.corruption_index
+    
 -- Calculate tourism revenue as percentage of GDP to identify most tourism dependent countries.
+
+SELECT 
+  t.country,
+  t.receipts_in_billions / rc.gdp_per_capita * 100 AS tourism_pct_gdp
+FROM tourism t
+INNER JOIN richest_countries rc
+  ON t.country = rc.country
+ORDER BY tourism_pct_gdp DESC
+
 -- Join cost of living and GDP per capita and calculate purchasing power index as percentage of GDP per capita
+
+SELECT
+  col.country,
+  col.purchasing_power_index / rc.gdp_per_capita * 100 AS purchase_power_pct_gdp
+FROM cost_of_living col
+INNER JOIN richest_countries rc
+  ON col.country = rc.country
+ORDER BY purchase_power_pct_gdp DESC
+
 -- Average corruption index by region
+
+SELECT
+  region,
+  AVG(c.corruption_index) AS avg_corruption_index
+FROM corruption c
+INNER JOIN countries cnt
+  ON c.country = cnt.country
+GROUP BY region
+ORDER BY avg_corruption_index
+
 -- Rank countries by receipts per tourist
+
+SELECT 
+  country,
+  receipts_per_tourist
+FROM tourism
+ORDER BY receipts_per_tourist DESC
+
 -- Unemployment rate differences between countries by year
+
+SELECT
+  c1.country AS country1,
+  c2.country AS country2,
+  c1.year,
+  c1.unemployment_rate AS rate1, 
+  c2.unemployment_rate AS rate2,
+  c1.unemployment_rate - c2.unemployment_rate AS rate_diff
+FROM unemployment c1
+INNER JOIN unemployment c2
+  ON c1.year = c2.year AND c1.country <> c2.country
+ORDER BY year
